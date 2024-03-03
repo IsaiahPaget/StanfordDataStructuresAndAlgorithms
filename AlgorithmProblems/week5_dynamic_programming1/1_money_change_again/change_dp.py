51520e4a90af03a891939b32b8496ba310731074
Isaiah Paget <113209222+IsaiahPaget@users.noreply.github.com>
2024-03-03 10:21:55 -0800

    dynamic programming money change problem

--- AlgorithmProblems/week5_dynamic_programming1/1_money_change_again/change_dp.py
+++ AlgorithmProblems/week5_dynamic_programming1/1_money_change_again/change_dp.py
@@ -1,8 +1,16 @@
 def change(money):
     # write your code here
+    coins = [1, 3, 4]
 
-    return money
+    dp = [money + 1] * (money + 1)
+    dp[0] = 0
 
+    for amount in range(1, money + 1):
+        for coin in coins:
+            if amount - coin >= 0:
+                dp[amount] = min(dp[amount], 1 + dp[amount - coin])
+
+    return dp[money] if dp[money] != money + 1 else -1
 
 if __name__ == '__main__':
     m = int(input())
